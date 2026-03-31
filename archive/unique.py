ad43d9f7a5e2d29db859beaf22e8e268ffc35c49
theraSara <s.saeedalhajeri@gmail.com>
2026-03-31 23:03:05 +0400

    Remove data.ipynb file

--- archive/unique.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/bk21_clean_trial_level.csv")
-
-stim_unique = df[["ITEM","condition","sentence","critical_word"]].drop_duplicates()
-
-print("Unique rows:", len(stim_unique))  # should be 648
-
-stim_unique.to_csv("data/bk21_unique_sentences.csv", index=False)
